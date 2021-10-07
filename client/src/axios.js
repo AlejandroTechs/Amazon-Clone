8b4f5de3d02031e2350b2402850d735e5bb44b85
Daniel Alejandro <alejandro.techservices@gmail.com>
2021-10-07 00:25:56 -0400

    front and backend built, with front end running as static folder from backend.

--- client/src/axios.js
+++ client/src/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const instance = axios.create({
     // THE API  URL at backend
     baseURL: 'http://localhost:3000/api/user' 
-    // http://localhost:5001/clone-ca463/us-central1/api
+ 
 }); 
 
 export default instance 
